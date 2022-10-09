4e2e437df9821f6566c307d7d0171f82fd15b98c
Yair Altman <altmany@gmail.com>
2022-10-09 17:32:51 +0300

    Fixes for issue #27

--- +did/+test/test_did_db_documents.m
+++ +did/+test/test_did_db_documents.m
@@ -8,35 +8,25 @@ function [b,msg] = test_did_db_documents(varargin)
 %  
 % This function first tries to delete a file 'test2.sqlite', and then
 % makes a new database with the same filename.
-%
-
-b = 1;
-msg = '';
 
 % Step 1: make an empty database with a starting branch
-!rm test_db_docs.sqlite
+delete test_db_docs.sqlite
 db = did.implementations.sqlitedb('test_db_docs.sqlite');
 db.add_branch('a');
-db.set_branch('a'); 
+db.set_branch('a'); % unnecessary - already done by db.add_branch('a')
 
 % Step 2: generate a set of documents with node names and a graph of the dependencies
-
 [G,node_names,docs] = did.test.documents.make_doc_tree([30 30 30]);
-
 
 figure;
 dG = digraph(G,node_names);
 plot(dG,'layout','circle');
-title(['The dependency relationships among the randomly generated documents.']);
+title('The dependency relationships among the randomly generated documents.');
 
-for i=1:numel(docs),
+for i=1:numel(docs)
 	db.add_doc(docs{i});
-end;
+end
 
 % Step 3: check the database results
-
- % not tested yet
-
+% not tested yet
 [b,msg] = did.test.documents.verify_db_document_structure(db, G, docs);
-
-
