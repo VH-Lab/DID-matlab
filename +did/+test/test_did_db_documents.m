becde392f1661c75aa6eee6a29059eb7e40b8987
Steve Van Hooser <vanhoosr@brandeis.edu>
2022-11-24 21:23:29 -0500

    modified test code for new get_parent_branch output, tested updates, closes #32 closes #35

--- +did/+test/test_did_db_documents.m
+++ +did/+test/test_did_db_documents.m
@@ -6,12 +6,16 @@ function [b,msg] = test_did_db_documents(varargin)
 % Tests the document adding functions of the did.database class, using the
 % did.implementations.sqlitedb class.
 %  
-% This function first tries to delete a file 'test_db_docs.sqlite', and then
-% makes a new database with the same filename.
+% This function first tries to delete a file 'test_db_docs.sqlite'
+% in the current directory, and then makes a new database with the same
+% filename.
 
 % Step 1: make an empty database with a starting branch
-delete test_db_docs.sqlite
-db = did.implementations.sqlitedb('test_db_docs.sqlite');
+db_filename = [pwd filesep 'test_db_docs.sqlite'];
+if isfile(db_filename),
+	delete(db_filename);
+end;
+db = did.implementations.sqlitedb(db_filename);
 db.add_branch('a');
 
 % Step 2: generate a set of documents with node names and a graph of the dependencies
@@ -22,9 +26,10 @@ dG = digraph(G,node_names);
 plot(dG,'layout','circle');
 title('The dependency relationships among the randomly generated documents.');
 
-for i=1:numel(docs)
-	db.add_doc(docs{i});
-end
+db.add_docs(docs);
+%for i=1:numel(docs)
+%	db.add_doc(docs{i});
+%end
 
 % Step 3: check the database results
 [b,msg] = did.test.documents.verify_db_document_structure(db, G, docs);
--- +did/+test/test_did_db_documents_branch.m
+++ +did/+test/test_did_db_documents_branch.m
@@ -10,8 +10,11 @@ function [b,msg] = test_did_db_documents_branch(varargin)
 % makes a new database with the same filename.
 
 % Step 1: make an empty database with a starting branch
-delete test_db_docs.sqlite
-db = did.implementations.sqlitedb('test_db_docs.sqlite');
+db_filename = [pwd filesep 'test_db_docs.sqlite'];
+if isfile(db_filename),
+	delete(db_filename); 
+end;
+db = did.implementations.sqlitedb(db_filename); 
 db.add_branch('a');
 
 % Step 2: generate a set of documents with node names and a graph of the dependencies
@@ -22,9 +25,10 @@ dG = digraph(G,node_names);
 plot(dG,'layout','circle');
 title('The dependency relationships among the randomly generated documents.');
 
-for i=1:numel(docs)
-	db.add_doc(docs{i});
-end
+db.add_docs(docs);
+%for i=1:numel(docs)
+%	db.add_doc(docs{i});
+%end
 
 % Step 3: now, add a new branch 'a_a'. The documents in the graph should be
 % accessible from the new branch 'a_a'.
@@ -46,7 +50,7 @@ for i=1:numel(docs),
     docs_to_remove{end+1} = docs{i}.id();
 end;
 
-db.remove_doc(docs_to_remove);
+db.remove_docs(docs_to_remove);
 
 db.set_branch('a');
 [b,msg] = did.test.documents.verify_db_document_structure(db, G, docs);
