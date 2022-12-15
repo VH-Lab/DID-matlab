70e566803ab44240c4bfd739a0e5c8a51ac37d4f
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2022-12-15 16:40:43 -0500

    updates to test_did_db_queries

--- +did/+test/+fun/get_docs_expected.m
+++ +did/+test/+fun/get_docs_expected.m
@@ -1,16 +1,32 @@
-function [docs_actual] = get_docs_expected(docs,id_expected,value_expected,query)
+function [ids_expected,docs_expected] = get_docs_expected(docs,id_input,value_input,query)
 %GET_DOCS_ACTUAL returns the expected document output of the selected query
-%   inputs: docs is a cell array of documents, id_actual is a string id,
-%   version_actual is a number, query is a string operation (either 'and'
-%   or 'or')
-%   outputs: docs_actual is a cell array of documents
-docs_actual = {};
+%
+%   [IDS_EXPECTED, DOCS_EXPECTED] = GET_DOCS_EXPECTED(DOCS, ID_INPUT, VALUE_INPUT, QUERY)
+%
+%   Given DOCS, a cell array of did.document objects, id_input (the id of a
+%   selected document), an array of VALUEs to search for in either demoA, demoB, or
+%   demoC, and query (either 'and' or 'or'), this calculates
+%   the expected output of the query.
+%
+%   IDS_EXPECTED is a cell array of the did.document ids that should be
+%   returned, and DOCS_EXPECTED is a cell array of did.document objects
+%   that match the query.
+%
+%   Example: 
+%      [ide,de] = get_docs_expected(docs, '12345',1,'and')
+%               % searches for a document that has a field
+%               % demoA.value equal to 1 or demoB.value equal to 1 or
+%               % demoC.value equal to 1 AND an id equal to '12345'.
+%
+%
+
+docs_expected = {};
 switch query
     case 'and'
         %first find docs that match the id
         docs_id_match = {};
         for doc_ind = 1:length(docs)
-            if strcmp(docs{doc_ind}.id,id_expected)
+            if strcmp(docs{doc_ind}.id,id_input)
                 docs_id_match{end+1} = docs{doc_ind};
             end
         end
@@ -18,27 +34,26 @@ switch query
         for doc_ind = 1:length(docs_id_match)
             demoType = did.test.fun.get_demoType(docs{doc_ind});
             each_value = eval(['docs{doc_ind}.document_properties.',demoType,'.value']);
-            if each_value == value_expected
-                docs_actual{end+1} = docs{doc_ind};
+            if did.datastructures.eqlen(each_value,value_input),
+                docs_expected{end+1} = docs{doc_ind};
             end
         end
     case 'or'
-        %first find docs that match the id
-        for doc_ind = 1:length(docs)
-            if strcmp(docs{doc_ind}.id,id_expected)
-                docs_actual{end+1} = docs{doc_ind};
-            end
-        end
         %keep adding docs that match the version to the list of docs that
         %match the id (but don't add those that already match id and are in the list)
         for doc_ind = 1:length(docs)
             demoType = did.test.fun.get_demoType(docs{doc_ind});
             each_value = eval(['docs{doc_ind}.document_properties.',demoType,'.value']);
-            if each_value == value_expected...
-                    && ~strcmp(docs{doc_ind}.id,id_expected)
-                docs_actual{end+1} = docs{doc_ind};
+            if ismember(each_value,value_input) | strcmp(docs{doc_ind}.id,id_input)
+                docs_expected{end+1} = docs{doc_ind};
             end
         end
-end
+end % switch
+
+ids_expected = {};
+for i=1:numel(docs_expected), 
+    ids_expected{i} = docs_expected{i}.id();
+end;
+
 end
 
--- +did/+test/test_did_db_queries.m
+++ +did/+test/test_did_db_queries.m
@@ -16,8 +16,6 @@ function [b,msg] = test_did_db_queries(varargin)
 % | Do_EXACTSTRING_test(1)| 0/1 Should we test exact_string                     |
 % | Do_AND_test (1)       | 0/1 Should we do the AND test?                      |
 % | Do_OR_test (1)        | 0/1 Should we do the OR test?                       |
-% | doc_id_ind (1)        | [1,n) Index of document whose id we're accessing    | 
-% | doc_value_ind (1)     | [1,n) Index of document whose value we're accessing | 
 % |-----------------------|-----------------------------------------------------|
 
 
@@ -26,9 +24,12 @@ function [b,msg] = test_did_db_queries(varargin)
 Do_EXACTSTRING_test = 1;
 Do_AND_test = 1;
 Do_OR_test = 1;
+did.datastructures.assign(varargin{:});
+
 doc_id_ind = 1;
 doc_value_ind = 1;
-did.datastructures.assign(varargin{:});
+doc_value_for_or = [10 11];
+
 % ***need to make sure the assigned values are within bounds: 
 % for tests, should be 0 or 1
 % for inds, should be between 1 and the number of documents produced 
@@ -142,42 +143,32 @@ if Do_AND_test,
     %d2 = db.search(did.query('base.id','exact_string',id_expected)&did.query('base.document_version','exact_number',value_expected));
     % ^ this was used when querying for document version using
     % 'exact_number'
-    docs_expected = did.test.fun.get_docs_expected(docs,id_expected,value_expected,'and'); %returns a cell array of docs that we expect the query to return
-    numDocs_expected = numel(docs_expected); %what is the expected number of documents?
+    [ids_expected,docs_expected] = did.test.fun.get_docs_expected(docs,id_expected,value_expected,'and'); %returns a cell array of docs that we expect the query to return
     exact_number_field_name = [demoType,'.value']; %set the first input of the query that uses the exact number operation
     d2 = db.search(did.query('base.id','exact_string',id_expected)&did.query(exact_number_field_name,'exact_number',value_expected));
     if ~iscell(d2) %can't do any of the below if the result of the search is not a cell with documents
         b = 0;
-        msg = ['AND operation query did not produce a cell array of documents - instead it produced an array of type ' class(d2) ' and length ' num2str(numel(d2)) '. Expected a cell array with ' num2str(numDocs_expected) ' document(s).'];
+        msg = ['AND operation query did not produce a cell array of documents - instead it produced an array of type ' class(d2) ' and length ' int2str(numel(d2)) '. Expected a cell array with ' int2str(numel(ids_expected)) ' document(s).'];
         disp(['This is the error; expected a cell array of documents.'])
+        disp(['We got:']);
+        d2,
+        disp(['We expected:'])
+        ids_expected,
+
 %         disp(['Setting d2 to be a cell array so we can continue']);
 %         d2 = {d2};
 %       not sure if ^ is the move, since there's no guarantee attaching
 %       curly brackets allows d2 to get through the next set of code
         return
-    end;
-    d2_docs = db.get_docs(d2);
-    numDocs_actual = numel(d2_docs);
-    
-    cond_numDocs = (numDocs_actual==numDocs_expected);
-    %check that there's the same number of documents from query as expected
-    if ~cond_numDocs
+    elseif ~did.datastructures.eqlen(d2,ids_expected)
         b = 0;
-        msg = ['Number of AND operation query documents does not equal the expected number: number of docs after query was ' numDocs_actual ' and number of docs expected was ' numDocs_expected '.'];
+        msg = ['AND operation query did not produce expected output.'];
+        disp(['We got:']);
+        d2,
+        disp(['We expected:'])
+        ids_expected,
         return
-    end
-    %check that the id is the same (assuming 1 document returned from query)
-    %***need to check that the size of d2_docs is 1 before accessing 1st index
-    if (numDocs_actual==1)
-        id_actual = d2_docs(1).id; 
-        cond_id = strcmp(id_actual,id_expected);
-        if ~cond_id
-            b = 0;
-            msg = ['AND operation query documents did not match the expected documents: id_actual was ' id_actual ' and id_expected was ' id_expected '.'];
-            return
-        end
-    end
-
+    end;
 
 end; % Do_AND_test
     
@@ -185,34 +176,26 @@ end; % Do_AND_test
 if Do_OR_test
     %check if number of documents the same
     exact_number_field_name = [demoType,'.value']; %set the first input of the query that uses the exact number operation
-    d3 = db.search(or(did.query('base.id','exact_string',id_expected),did.query(exact_number_field_name,'exact_number',value_expected)));
-    d3_docs = db.get_docs(d3);
-    numDocs_actual = numel(d3_docs);
-    docs_expected = did.test.fun.get_docs_expected(docs,id_expected,value_expected,'or'); 
-    numDocs_expected = numel(docs_expected);
-    if numDocs_actual~=numDocs_expected
+    d3 = db.search(or(did.query('base.id','exact_string',id_expected),or(did.query(exact_number_field_name,'exact_number',doc_value_for_or(1)),did.query(exact_number_field_name,'exact_number',doc_value_for_or(2)))));
+    [ids_expected,docs_expected] = did.test.fun.get_docs_expected(docs,id_expected,doc_value_for_or,'or');     
+    if ~iscell(d3) %can't do any of the below if the result of the search is not a cell with documents
         b = 0;
-        msg = ['Number of or operation query documents did not match the expected number of documents: number of docs after query was ' num2str(numDocs_actual) ' and number of docs expected was ' num2str(numDocs_expected) '.'];
+        msg = ['OR operation query did not produce a cell array of documents - instead it produced an array of type ' class(d3) ' and length ' int2str(numel(d3)) '. Expected a cell array with ' int2str(numel(docs_expected)) ' document(s).'];
+        disp(['This is the error; expected a cell array of documents.'])
+        disp(['We got:']);
+        d3,
+        disp(['We expected:'])
+        ids_expected,
+        return;
+    elseif ~did.datastructures.eqlen(d3(:),ids_expected(:))
+        b = 0;
+        msg = ['OR operation query did not produce expected output.'];
+        disp(['We got:']);
+        d3,
+        disp(['We expected:'])
+        ids_expected,
         return
-    end
-    % check for each expected id, there is a matching actual id
-
-    for expected_ind = 1:numDocs_expected
-        id_expected = docs_expected{expected_ind}.id;
-        b_pre = 0; %for each id, assume b_pre is false unless a match is found
-        for pred_ind = 1:numDocs_actual
-            id_actual = d3_docs(pred_ind).id;
-            if strcmp(id_expected,id_actual)
-                b_pre = 1;
-            end
-        end
-        if b_pre==0 %if no match is found for any of the expected ids
-            b = 0;
-            msg = ['Query failed to produce document with id: ' id_expected,'.'];
-        end
-    end
+    end;
     
 end % Do_OR_test
-
-
 
