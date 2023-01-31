fba6cbb85fd51d2b6c4e4db279d131afe062c866
Avi Lepsky <avilepsky@brandeis.edu>
2023-01-31 18:05:00 -0500

    updates to invalidation test code

--- +did/+test/+documents/make_doc_tree_invalid.m
+++ +did/+test/+documents/make_doc_tree_invalid.m
@@ -1,7 +1,7 @@
-function [G, node_names, docs] = make_doc_tree_invalid(rates, method)
+function [G, node_names, docs] = make_doc_tree_invalid(rates,varargin)
 % MAKE_DOC_TREE - make a "tree" of documents to add to a database
 %
-% [G, NODE_NAMES, DOCS] = MAKE_DOC_TREE_INVALID(RATES, METHOD)
+% [G, NODE_NAMES, DOCS] = MAKE_DOC_TREE_INVALID(RATES, METHODS)
 %
 % Makes a directed graph G, associated NODE_NAMES, and demo did.documents 
 % DOCS by generating documents of type demoA demoB and demoC at poisson
@@ -42,11 +42,38 @@ ids_B = {};
 ids_C = {};
 
 
+%list of possible tests and their default option
+value_modifier = 'sham';
+id_modifier = 'sham';
+datestamp_modifier = 'sham';
+session_id_modifier= 'sham';
+dependency_modifier = 'sham'; % primarily for demoC
+remover = 'sham';
+did.datastructures.assign(varargin{:});
+
 
 for i=1:numA,
-	docs{end+1} = did.document('demoA','demoA.value',modifyvalue(method,counter));
+	docs{end+1} = did.document('demoA'); %add unmodified document to list
+    %now can continue modifying docs:
+    d = docs{end};
+    d_struct = struct(d);
+    %modify value:
+    d_struct.document_properties.demoA.value = modifyvalue(value_modifier,counter);
+    %modify id: 
+    current_id = docs{end}.document_properties.base.id; 
+    d_struct.document_properties.base.id = modifyid(id_modifier,current_id);
+    %remove a struct or field:
+    d_struct = remove(remover,d_struct);
+    %finish:
+    if isfield(d_struct,'document_properties')
+        docs{end} = did.document(d_struct.document_properties); %replace document in list with the modified version
+    else
+        docs{end} = did.document(d_struct);
+    end
 	node_names{end+1} = int2str(counter);
-	ids_A{end+1} = docs{end}.id();
+    if isfield(docs{end}.document_properties,'base') && isfield(docs{end}.document_properties.base,'id')
+        ids_A{end+1} = docs{end}.id();
+    end
 	counter = counter + 1;
 end;
 
@@ -54,7 +81,9 @@ for i=1:numB,
 	docs{end+1} = did.document('demoB','demoB.value',int2str(counter),...
 		'demoA.value',counter);
 	node_names{end+1} = int2str(counter);
-	ids_B{end+1} = docs{end}.id();
+    if isfield(docs{end}.document_properties.base,'id')
+        ids_B{end+1} = docs{end}.id();
+    end
 	counter = counter + 1;
 end;
 
@@ -68,7 +97,7 @@ for i=1:numC,
 	docs{end+1} = did.document('demoC','demoC.value',int2str(counter));
 	node_names{end+1} = int2str(counter);
 	ids_C{end+1} = docs{end}.id();
-	if depA>0,
+	if depA>0 && numel(ids_A)>0 % check that ids_A is being filled in before accessing its indices 
 		docs{end} = docs{end}.set_dependency_value('item1',...
 			ids_A{depA});
 		G(depA,counter) = 1;
@@ -91,24 +120,84 @@ end;
 
 function value = modifyvalue(method, value)
 
+    switch method
+        case 'int2str'
+            value = int2str(value);
+        case 'blank int'
+            value = [];
+        case 'blank str'
+            value = '';
+        case 'nan'
+            value = nan;
+        case 'double'
+            value = value + .5;
+        case 'too negative'
+            value = -1 * intmax;
+        case 'too positive'
+            value = intmax;
+        case 'sham'
+        otherwise
+            error(['Unknown method ' method '.']);    
+    end;
+
+function id = modifyid(method,id)
 switch method
-    case 'int2str',
-        value = int2str(value);
-    case 'blank int'
-        value = [];
-    case 'blank str'
-        value = '';
-    case 'nan'
-        value = nan;
-    case 'double'
-        value = value + .5;
-    case 'too negative'
-        value = -1 * intmax;
-    case 'too positive'
-        value = intmax;
+    case 'substring'
+        id = cell2mat(extractBetween(id,1,32));
+    case 'replace_underscore'
+        id = replaceBetween(id,17,17,'a'); %replace underscore with the letter a
+    case 'add'
+        id = [id 'a']; %add the letter a to the end of the id
+    case 'replace_letter_valid'
+        id = replaceBetween(id,1,1,'a'); %replace letter/digit with another letter
+    case 'replace_letter_invalid1'
+        id = replaceBetween(id,1,1,'*'); %replace letter/digit with a special character
+    case 'replace_letter_invalid2'
+        id = replaceBetween(id,1,1,''''); %replace letter/digit with a special charactercase 'sham'
     case 'sham'
-    otherwise,
-        error(['Unknown method ' method '.']);    
-end;
+    otherwise
+        error(['Unknown method ' method '.']);
+end
+function struct = remove(method,struct)
+switch method
+    case 'document_properties'
+        struct = rmfield(struct,'document_properties');
+    case 'base'
+        struct.document_properties = rmfield(struct.document_properties,'base');
+    case 'session_id'
+        struct.document_properties.base = rmfield(struct.document_properties.base,'session_id');
+    case 'id'
+        struct.document_properties.base = rmfield(struct.document_properties.base,'id');
+    case 'name'
+        struct.document_properties.base = rmfield(struct.document_properties.base,'name');
+    case 'datestamp'
+        struct.document_properties.base = rmfield(struct.document_properties.base,'datestamp');
+    case 'demoA'
+        struct.document_properties = rmfield(struct.document_properties,'base');
+    case 'demoB' %for demoB docs
+    case 'demoC' %for demoC docs
+    case 'value'
+        struct.document_properties.demoA = rmfield(struct.document_properties.demoA,'value');
+    case 'document_class'
+        struct.document_properties = rmfield(struct.document_properties,'document_class');
+    case 'definition'
+        struct.document_properties.document_class = rmfield(struct.document_properties.document_class,'definition');
+    case 'validation'
+        struct.document_properties.document_class = rmfield(struct.document_properties.document_class,'validation');
+    case 'class_name'
+        struct.document_properties.document_class = rmfield(struct.document_properties.document_class,'class_name');
+    case 'property_list_name'
+        struct.document_properties.document_class = rmfield(struct.document_properties.document_class,'property_list_name');
+    case 'class_version'
+        struct.document_properties.document_class = rmfield(struct.document_properties.document_class,'class_version');
+    case 'superclasses'
+        struct.document_properties.document_class = rmfield(struct.document_properties.document_class,'superclasses');
+    case 'superclasses.definition'
+        struct.document_properties.document_class.superclasses = rmfield(struct.document_properties.document_class.superclasses,'definition');
+    case 'sham'
+    otherwise
+        error(['Unknown method ' method '.']);
+end
+    
 
 
--- +did/+test/test_did_db_documents_invalid.m
+++ +did/+test/test_did_db_documents_invalid.m
@@ -19,9 +19,20 @@ db = did.implementations.sqlitedb(db_filename);
 db.add_branch('a');
 
 % Step 2: generate a set of documents with node names and a graph of the dependencies
-modifier = 'sham';
+value_modifier = 'sham';
+id_modifier = 'sham';
+datestamp_modifier = 'sham';
+session_id_modifier= 'sham';
+dependency_modifier = 'sham'; % primarily for demoC
+remover = 'sham'; 
 did.datastructures.assign(varargin{:});
-[G,node_names,docs] = did.test.documents.make_doc_tree_invalid([30 30 30],modifier);
+[G,node_names,docs] = did.test.documents.make_doc_tree_invalid([30 30 30],... 
+    'value_modifier',value_modifier,... %add assign varargin to make_doc_tree_invalid
+    'id_modifier',id_modifier,...
+    'datestamp_modifier', datestamp_modifier,...
+    'session_id_modifier', session_id_modifier,...
+    'dependency_modifier', dependency_modifier,...
+    'remover',remover);
 
 figure;
 dG = digraph(G,node_names);
@@ -32,14 +43,20 @@ try
     db.add_docs(docs);
 catch E
     b = 1;
-    msg = [E.message newline 'Error due to the following modifier: ' '''' modifier ''''];
+    msg = [E.message newline 'Error due to one of the following modifiers: '...
+        newline 'value_modifier:' '''' value_modifier ''''...
+        newline 'id_modifier:' '''' id_modifier ''''...
+        newline 'datestamp_modifier:' '''' datestamp_modifier ''''...
+        newline 'session_id_modifier:' '''' session_id_modifier ''''...
+        newline 'dependency_modifier:' '''' dependency_modifier ''''...
+        newline 'remover:' '''' remover ''''];
     disp(msg);
     return
 end
-% db.add_docs(docs);
-%for i=1:numel(docs)
-%	db.add_doc(docs{i});
-%end
+%db.add_docs(docs);
+% for i=1:numel(docs)
+% 	db.add_doc(docs{i});
+% end
 
 % Step 3: check the database results
 [b,msg] = did.test.documents.verify_db_document_structure(db, G, docs);
