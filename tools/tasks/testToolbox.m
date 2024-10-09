6f2e933a7105ef51990ab1dcef07afee1074001b
ehennestad <ehennestad@gmail.com>
2024-10-09 02:45:21 -0500

    install requirements before running tests

--- tools/tasks/testToolbox.m
+++ tools/tasks/testToolbox.m
@@ -1,6 +1,7 @@
 function testToolbox(varargin)
     installMatBox()
     projectRootDirectory = didtools.projectdir();
+    matbox.installRequirements(projectRootDirectory)
     sourceFolderName = '+did';
     matbox.tasks.testToolbox(projectRootDirectory, varargin{:}, 'SourceFolderName', sourceFolderName)
 end
