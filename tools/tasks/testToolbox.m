35fc732bb465df5f59b66ea44b2dc7be77b9c67d
ehennestad <ehennestad@gmail.com>
2024-10-11 10:12:26 -0500

    Update testToolbox.m

--- tools/tasks/testToolbox.m
+++ tools/tasks/testToolbox.m
@@ -2,5 +2,9 @@ function testToolbox(varargin)
     installMatBox()
     projectRootDirectory = didtools.projectdir();
     matbox.installRequirements(projectRootDirectory)
+    
+    tf = isfile('/home/runner/work/DID-matlab/DID-matlab/MATLAB-AddOns/mksqlite-master/mksqlite.mexa64');
+    fprintf("File is present: %d\n", tf)
+
     matbox.tasks.testToolbox(projectRootDirectory, varargin{:})
 end
