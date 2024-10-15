54e62629cf7e6939917daeb6c99006db650e2e81
ehennestad <ehennestad@gmail.com>
2024-10-15 11:48:55 +0200

    Update testToolbox.m
    Remove debug print statement

--- tools/tasks/testToolbox.m
+++ tools/tasks/testToolbox.m
@@ -3,8 +3,5 @@ function testToolbox(varargin)
     projectRootDirectory = didtools.projectdir();
     matbox.installRequirements(projectRootDirectory)
     
-    tf = isfile('/home/runner/work/DID-matlab/DID-matlab/MATLAB-AddOns/mksqlite-master/mksqlite.mexa64');
-    fprintf("File is present: %d\n", tf)
-
     matbox.tasks.testToolbox(projectRootDirectory, varargin{:})
 end
