076f635ae1691f835e6d905abc9ae0de14653a6b
ehennestad <ehennestad@gmail.com>
2024-10-22 10:31:25 +0200

    Update mustBeValidPermission.m
    Add text mode to valid permissions

--- code/+did/+file/mustBeValidPermission.m
+++ code/+did/+file/mustBeValidPermission.m
@@ -6,8 +6,11 @@ function mustBeValidPermission(value)
     if ismissing(value); return; end
 
     VALID_PERMISSIONS = ["r", "w", "a", "r+", "w+", "a+", "W", "A"];
-
+    
     permissionsAsString = strjoin( "  " + VALID_PERMISSIONS, newline);
+
+    % Add text modes:
+    VALID_PERMISSIONS = [VALID_PERMISSIONS, insertAfter(VALID_PERMISSIONS, 1, "t")];
 
     assert(ismember(value, VALID_PERMISSIONS), ...
         'File permission must be one of:\n%s\n', permissionsAsString)
