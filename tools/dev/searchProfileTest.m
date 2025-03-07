ea3e0da12fe630d42c00525eafbefb2a0b3bfd0b
Steve Van Hooser <vanhoosr@brandeis.edu>
2025-03-06 20:00:40 -0500

    searchProfileTest

---
+++ tools/dev/searchProfileTest.m
@@ -0,0 +1,79 @@
+function db = searchProfileTest(db, test_number)
+% SEARCHPROFILETEST - perform a profiling test of querying DID databases
+%
+% DB = SEARCHPROFILETEST(DB, TEST_NUMBER)
+%
+% Create an example database and test query speed.
+%
+% TEST_NUMBER == 1:
+%    Search and retrieve all documents of one type
+% TEST_NUMBER == 2:
+%    Search and retrieve all documents of one type
+%    Perform a more complicated query
+% TEST_NUMBER == 3:
+%    Search and retrieve all documents of one type
+%    Perform a more complicated query
+%    Perform a second more complicated query
+% 
+% 
+% Example 1:
+%    db = searchProfileTest();
+%    profile on;
+%    searchProfileTest(db,1);
+%    profile off;
+%    profile viewer;
+%
+% Example 2:
+%    profile on;
+%    searchProfileTest(db,2);
+%    profile off;
+%    profile viewer;
+%
+% Example 3:
+%    profile on;
+%    searchProfileTest(db,3);
+%    profile off;
+%    profile viewer;
+%
+
+if nargin<1
+    % need to create database
+    db_filename = tempname;
+    [G,node_names,docs] = did.test.helper.documents.make_doc_tree([1e4 1e4 1e4]);
+    db = did.implementations.sqlitedb(db_filename);
+    db.add_branch('a');
+    db.add_docs(docs);
+    return;
+end
+
+q1 = did.query('','isa','demoB');
+
+demoBdocID = db.search(q1);
+demoBdocs = db.get_docs(demoBdocID);
+
+if test_number == 1
+    return;
+end;
+
+q2 = ndi.query('demoB.value','exact_number',demoBdocs(5).document_properties.demoB.value);
+
+match_ids = db.search(q1&q2);
+match_docs = db.get_docs(match_ids);
+
+if test_number == 2
+    return;
+end
+
+qC = ndi.query('','isa','demoC');
+demoCdocID = db.search(qC);
+demoCdocs = db.get_docs(demoCdocID);
+
+q3 = ndi.query('','depends_on','*',demoBdocs(5).document_properties.base.id);
+
+match_ids2 = db.search(q3);
+match_docs2 = db.get_docs(match_ids2);
+
+
+
+
+          
