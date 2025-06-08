5039bfc42949cc9de0d318c2f6c305724b6a1940
ehennestad <ehennestad@gmail.com>
2025-06-08 20:34:13 +0200

    Update matbox task function overrides

--- tools/tasks/createTestedWithBadgeforToolbox.m
+++
@@ -1,7 +0,0 @@
-function createTestedWithBadgeforToolbox(versionNumber)
-    arguments
-        versionNumber (1,1) string
-    end
-    projectRootDirectory = didtools.projectdir();
-    matbox.tasks.createTestedWithBadgeforToolbox(versionNumber, projectRootDirectory)
-end
