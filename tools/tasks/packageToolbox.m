06fe3692097d2b039a40195d6352a22172471a25
ehennestad <ehennestad@gmail.com>
2025-07-25 13:01:26 +0200

    Delete packageToolbox.m

--- tools/tasks/packageToolbox.m
+++
@@ -1,9 +0,0 @@
-function [newVersion, mltbxPath] = packageToolbox(releaseType, versionString)
-    arguments
-        releaseType {mustBeTextScalar,mustBeMember(releaseType,["build","major","minor","patch","specific"])} = "build"
-        versionString {mustBeTextScalar} = "";
-    end
-    projectRootDirectory = didtools.projectdir();
-    [newVersion, mltbxPath] = matbox.tasks.packageToolbox(projectRootDirectory, ...
-        releaseType, versionString, "ToolboxShortName", "DID_MATLAB");
-end
